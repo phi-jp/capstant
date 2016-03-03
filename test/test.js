2162d33b5d720088a74792c00d798dffc43f5a88
phi <phi1618jp@gmail.com>
2016-03-03 19:49:38 +0900

    fix test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 
 var capstant = require('../lib/capstant');
 
-capstant.shot('http://phiary.me', './output/phiary.png', {
-  phantomPath: 'slimerjs',
-  delay: 2000,
+capstant.shot('http://google.com', './output/google.png').then(function(url) {
+  console.log(arguments);
+});
+
+capstant.shot('http://phiary.me', './output/phiary1.png', {
+  delay: 1200,
   width: 320,
   height: 240,
   viewportSize: {
@@ -14,8 +17,11 @@ capstant.shot('http://phiary.me', './output/phiary.png', {
   console.log(arguments);
 });
 
-capstant.shot('http://google.com', './output/google.png', {
+capstant.shot('http://phiary.me', './output/phiary2.png', {
   phantomPath: 'slimerjs',
+  delay: 1200,
+  width: 320,
+  height: 240,
   viewportSize: {
     width: 640,
     height: 480,
