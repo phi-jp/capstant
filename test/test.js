8648430322340c0df0cbddee000f99fa2e90da28
phi <phi1618jp@gmail.com>
2016-03-03 19:25:29 +0900

    fix

--- test/test.js
+++ test/test.js
@@ -4,12 +4,10 @@ var capstant = require('../lib/capstant');
 capstant.shot('http://phiary.me', './hoge.png', {
   phantomPath: 'slimerjs',
   delay: 2000,
+  width: 320,
+  height: 240,
   viewportSize: {
     width: 640,
     height: 480,
-  },
-  size: {
-    width: 320,
-    height: 240,
   },
 });
